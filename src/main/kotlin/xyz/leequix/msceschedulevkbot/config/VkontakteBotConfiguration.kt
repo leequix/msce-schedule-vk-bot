8987f1575b100ac398c418a2a1742423aa6a64fd
leequix <leequix@yandex.com>
2018-03-25 18:42:33 +0300

    Created message processing service

--- src/main/kotlin/xyz/leequix/msceschedulevkbot/config/VkontakteBotConfiguration.kt
+++ src/main/kotlin/xyz/leequix/msceschedulevkbot/config/VkontakteBotConfiguration.kt
@@ -1,9 +1,12 @@
 package xyz.leequix.msceschedulevkbot.config
 
 import com.petersamokhin.bots.sdk.clients.Group
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import xyz.leequix.msceschedulevkbot.service.MessageProcessingService
+import xyz.leequix.msceschedulevkbot.service.UserService
 
 @Configuration
 class VkontakteBotConfiguration {
@@ -13,12 +16,19 @@ class VkontakteBotConfiguration {
     @Value("\${vk.group.token}")
     private val groupToken: String = ""
 
+    @Autowired
+    private lateinit var userService: UserService
+
+    @Autowired
+    private lateinit var messageProcessingService: MessageProcessingService
+
     @Bean
     fun getGroup(): Group {
         val group = Group(groupId, groupToken)
 
         group.onMessage {
-            println("${it.authorId()}: ${it.text}")
+            val user = userService.getOrCreateUser(it.authorId())
+            messageProcessingService.onGetMessage(user, it)
         }
 
         return group
---
+++ src/main/kotlin/xyz/leequix/msceschedulevkbot/service/MessageProcessingService.kt
@@ -0,0 +1,12 @@
+package xyz.leequix.msceschedulevkbot.service
+
+import com.petersamokhin.bots.sdk.objects.Message
+import org.springframework.stereotype.Service
+import xyz.leequix.msceschedulevkbot.model.User
+
+@Service
+class MessageProcessingService {
+    fun onGetMessage(user: User, message: Message) {
+
+    }
+}
