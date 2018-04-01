d838be7c70a1e84558bc2c515590d9f37d923b1d
leequix <leequix@yandex.com>
2018-04-01 23:53:08 +0300

    Refactored MainMenu

--- src/main/kotlin/xyz/leequix/msceschedulevkbot/menu/MainMenu.kt
+++ src/main/kotlin/xyz/leequix/msceschedulevkbot/menu/MainMenu.kt
@@ -1,17 +1,47 @@
 package xyz.leequix.msceschedulevkbot.menu
 
+import com.petersamokhin.bots.sdk.callbacks.callbackapi.ExecuteCallback
+import com.petersamokhin.bots.sdk.objects.Message
 import xyz.leequix.msceschedulevkbot.model.User
 
 class MainMenu : Menu() {
     override val menuId = MENU_ID
 
-    override fun getMenuElements() = listOf("Привет", "Пока", "Эммм")
+    override fun getMenuElements() =
+            listOf(
+                    "Подписаться на группу",
+                    "Отписаться от группы"
+            )
 
     override fun getMenuHeader() = "Выбирай"
 
     override fun onSelected(user: User, elementIndex: Int) = when(elementIndex) {
         1 -> {
-
+            Message()
+                    .title("Ошибка")
+                    .text("Not implemented")
+                    .from(group)
+                    .to(user.vkontakteId)
+                    .send()
+        }
+        2 -> {
+            if (user.group === null) {
+                Message()
+                        .title("Ошибка")
+                        .text("Вы не подписаны ни на одну группу")
+                        .from(group)
+                        .to(user.vkontakteId)
+                        .send()
+            } else {
+                Message()
+                        .title("Успех")
+                        .text("Вы отписались от группы ${user.group!!.number}")
+                        .from(group)
+                        .to(user.vkontakteId)
+                        .send()
+                user.group = null
+                userService.save(user)
+            }
         }
         else -> sendMessageAboutUnknownElementIndex(user)
     }
