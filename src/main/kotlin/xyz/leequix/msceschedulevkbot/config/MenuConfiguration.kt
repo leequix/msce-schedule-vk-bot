cbf7f29714f8cf33c1cf5c021d2ceb28540ba342
leequix <leequix@yandex.com>
2018-04-01 18:14:49 +0300

    Created Spring configuration for Menu Abstract Class

---
+++ src/main/kotlin/xyz/leequix/msceschedulevkbot/config/MenuConfiguration.kt
@@ -0,0 +1,35 @@
+package xyz.leequix.msceschedulevkbot.config
+
+import com.petersamokhin.bots.sdk.clients.Group
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Lazy
+import org.springframework.context.annotation.Scope
+import xyz.leequix.msceschedulevkbot.menu.MainMenu
+import xyz.leequix.msceschedulevkbot.menu.Menu
+import xyz.leequix.msceschedulevkbot.menu.NullMenu
+import xyz.leequix.msceschedulevkbot.service.UserService
+
+@Configuration
+class MenuConfiguration {
+    @Autowired
+    private lateinit var group: Group
+
+    @Autowired
+    private lateinit var userService: UserService
+
+    @Bean
+    @Scope("prototype")
+    @Lazy(true)
+    fun getMenuById(id: String): Menu {
+        val menu = when (id) {
+            MainMenu.MENU_ID -> MainMenu()
+            else -> NullMenu()
+        }
+        menu.group = group
+        menu.userService = userService
+
+        return menu
+    }
+}
