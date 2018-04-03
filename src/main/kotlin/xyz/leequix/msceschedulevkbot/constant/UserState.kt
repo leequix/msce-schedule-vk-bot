c4a870f25ac5f06a0d7a3be3b14f04e65fa595ba
leequix <leequix@yandex.com>
2018-04-03 19:34:52 +0300

    Created new state

--- src/main/kotlin/xyz/leequix/msceschedulevkbot/constant/UserState.kt
+++ src/main/kotlin/xyz/leequix/msceschedulevkbot/constant/UserState.kt
@@ -2,5 +2,6 @@ package xyz.leequix.msceschedulevkbot.constant
 
 enum class UserState(val state: String) {
     STATUS("status"),
-    MENU("menu")
+    MENU("menu"),
+    MENU_ELEMENTS("menu elements")
 }
