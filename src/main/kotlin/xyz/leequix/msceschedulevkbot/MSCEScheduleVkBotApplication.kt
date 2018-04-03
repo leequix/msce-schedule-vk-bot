4cece8905e3926bc70ef3b0b42775d54b01f750b
leequix <leequix@yandex.com>
2018-04-03 18:05:16 +0300

    Enabled scheduling

--- src/main/kotlin/xyz/leequix/msceschedulevkbot/MSCEScheduleVkBotApplication.kt
+++ src/main/kotlin/xyz/leequix/msceschedulevkbot/MSCEScheduleVkBotApplication.kt
@@ -2,8 +2,10 @@ package xyz.leequix.msceschedulevkbot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class MSCEScheduleVkBotApplication
 
 fun main(args: Array<String>) {
