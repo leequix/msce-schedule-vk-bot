56b9fa253a167d85e4ce2959fa5e46be09487de4
leequix <leequix@yandex.com>
2018-04-03 19:34:24 +0300

    Created new method

--- src/main/kotlin/xyz/leequix/msceschedulevkbot/repository/GroupRepositroy.kt
+++ src/main/kotlin/xyz/leequix/msceschedulevkbot/repository/GroupRepositroy.kt
@@ -2,7 +2,9 @@ package xyz.leequix.msceschedulevkbot.repository
 
 import org.springframework.data.mongodb.repository.MongoRepository
 import xyz.leequix.msceschedulevkbot.model.Group
+import java.util.*
 
 interface GroupRepositroy : MongoRepository<Group, String> {
     fun existsByNumber(number: String): Boolean
+    fun findByNumber(number: String): Optional<Group>
 }
